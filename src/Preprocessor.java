b6be0b417ceddb8acd449692b382d16fe77d68e7
Nikos Aréchiga <narechig@ece.cmu.edu>
2013-11-01 17:04:28 -0400

    Preprocessor to find mode declarations completed

--- src/Preprocessor.java
+++ src/Preprocessor.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.LinkedList;
-import java.util.regex;
+import java.util.regex.*;
 
 class Preprocessor {
 
@@ -17,22 +17,42 @@ class Preprocessor {
 		this.inputReader.close();
 	}
 
-	public LinkedList<String> extractStableModes() {
+	public LinkedList<String> extractStableModes() throws IOException {
+
+		boolean cont = true;
 		String nextLine;
-		LinkedList<String> stableModes;
+		LinkedList<String> stableModes = new LinkedList<String>();
 
-		Pattern declarationPattern  = Pattern.compile("//#stablemode<(.*)>");
-
+		Pattern declarationPattern  = Pattern.compile("// #stablemode<M(\\d)*>");
+		Pattern modePattern = Pattern.compile("M(\\d)*");
+		
 		nextLine = inputReader.readLine();
-		Matcher matcher = declarationPattern( nextLine );
+		while ( nextLine != null ) {
+			Matcher declarationMatcher = declarationPattern.matcher( nextLine );
 
-		while matcher.find()
+			System.out.println("Line was: " + nextLine );
 
+			if ( declarationMatcher.find() ) {
+				String declaration = declarationMatcher.group();
+				int start = declarationMatcher.start();
+				int end = declarationMatcher.end();
+				String mode;
+
+				System.out.println("Found declaration match " + declaration );
+				System.out.println("Starting at " + start );
+				System.out.println("Ending at " + end );
+
+				Matcher modeMatcher = modePattern.matcher( declaration );
+				modeMatcher.find();
+				mode = modeMatcher.group();
+				System.out.println("Found mode " + mode );
+				stableModes.addLast( mode );
+			} 
+			nextLine = inputReader.readLine();
+
+		}
 		return stableModes;
-	
 	}
-
-
 }
 
 
