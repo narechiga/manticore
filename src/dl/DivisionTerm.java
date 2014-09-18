2c78844687de6a8a572047ecc2e696cf3fc32bdf
Nikos Aréchiga <narechig@ece.cmu.edu>
2014-09-17 17:35:35 -0700

    Currently working on the isLinear and isAffine methods
    Later, will build a simple arithmetic solver interface. The goal is
    to extract the linear or affine dynamics blocks and "easily" generate
    ivnariants for them, for use with forward invariant cuts.
    
    Currently have finished only Addition and Division, was working on
    Mulitplication.

--- src/dl/DivisionTerm.java
+++ src/dl/DivisionTerm.java
@@ -18,6 +18,14 @@ public class DivisionTerm extends Term {
 		return (Term)getChild( 1 );
 	}
 
+	public Term getNumerator() {
+		return (Term)getChild( 0 );
+	}
+
+	public Term getDenominator() {
+		return (Term)getChild( 1 );
+	}
+
 	public Term getLHS() {
 		return (Term)getChild( 0 );
 	}
@@ -26,6 +34,53 @@ public class DivisionTerm extends Term {
 		return (Term)getChild( 1 );
 	}
 
+	public boolean isLinearIn( ArrayList<RealVariable> variables ) {
+		// If the dividend is linear and the denominator does not contain
+		// the variables, then it is linear.
+
+		boolean linearity;
+
+		if ( getNumerator().isLinearIn( variables ) ) {
+			linearity = true;
+			
+			// Now check if any of the variables appears in the denom
+			Set<RealVariable> denomVars = getDenominator().getFreeVariables();
+			int denomVarsCardinality = denomVars.size();
+			denomVars.removeAll( variables );
+			if ( denomVars.size() < denomVarsCardinality ) {
+				linearity = false;
+			}
+
+		} else {
+			linearity = false;
+		}
+
+		return linearity;
+	}
+
+	public boolean isAffineIn( ArrayList<RealVariable> variables ) {
+		// If the dividend is affine and the denominator does not contain
+		// the variables, then it is affine.
+
+		boolean affinity;
+
+		if ( getNumerator().isAffineIn( variables ) ) {
+			affinity = true;
+			
+			// Now check if any of the variables appears in the denom
+			Set<RealVariable> denomVars = getDenominator().getFreeVariables();
+			int denomVarsCardinality = denomVars.size();
+			denomVars.removeAll( variables );
+			if ( denomVars.size() < denomVarsCardinality ) {
+				affinity = false;
+			}
+
+		} else {
+			affinity = false;
+		}
+
+		return affinity;
+	}
 
 }
 
--- src/dl/MultiplicationTerm.java
+++ src/dl/MultiplicationTerm.java
@@ -39,5 +39,41 @@ public class MultiplicationTerm extends Term {
 	public MultiplicationTerm clone() {
 		return new MultiplicationTerm( getLeftFactor(), getRightFactor() );
 	}
+
+// Arithmetic
+	public boolean isLinearIn( ArrayList<RealVariable> variables ) {
+		// If exactly one of the factors contains any of the variables
+		// of interest, then the product is linear
+
+		boolean linearity;
+
+
+
+
+	}
+
+	public boolean isAffineIn( ArrayList<RealVariable> variables ) {
+		// If the dividend is affine and the denominator does not contain
+		// the variables, then it is affine.
+
+		boolean affinity;
+
+		if ( getNumerator().isAffineIn( variables ) ) {
+			affinity = true;
+			
+			// Now check if any of the variables appears in the denom
+			Set<RealVariable> denomVars = getDenominator().getFreeVariables();
+			int denomVarsCardinality = denomVars.size();
+			denomVars.removeAll( variables );
+			if ( denomVars.size() < denomVarsCardinality ) {
+				affinity = false;
+			}
+
+		} else {
+			affinity = false;
+		}
+
+		return affinity;
+	}
 }
 
